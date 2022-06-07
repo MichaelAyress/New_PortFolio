ac19c6bfec3c7dbffe6e537d3d4b8432701b25b1
Michael Ayres <99827730+MichaelAyress@users.noreply.github.com>
2022-06-07 11:51:37 -0300

    8.0

--- main.js
+++ main.js
@@ -14,3 +14,18 @@ setInterval(function () {
   
     next.setAttribute("data-show", "");
   }, 2000);
+
+  function toggle() {
+    let links = document.getElementById("links");
+    let blob = document.getElementById("blob");
+    blob.classList.toggle("open");
+    if (links.style.display == "block") {
+      links.style.display = "none";
+    } else {
+      links.style.display = "block";
+    }
+  }
+
+  function abrirLink() {
+    window.location.href = "https://www.canva.com/design/DAE7FWZDiww/z1joiZjLR8sZruZ_x6kNqQ/view?utm_content=DAE7FWZDiww&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink"
+  }
